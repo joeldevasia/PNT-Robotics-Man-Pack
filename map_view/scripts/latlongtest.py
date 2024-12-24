de4d17e273515a0eed58952d99b772cdcd75a668
Joel Devasia <joeldevasia1962002@gmail.com>
2024-12-24 12:57:37 +0530

    Remove All unncecessary files

--- map_view/scripts/latlongtest.py
+++
@@ -1,4 +0,0 @@
-import utm
-
-latitude_longitude = utm.to_latlon(easting=572132.39, northing=3457839.01, zone_number=43, northern=False)
-print(latitude_longitude[0], latitude_longitude[1])
--- map_view/scripts/wmctrl_test.py
+++
@@ -1,9 +0,0 @@
-import wmctrl
-
-# Get the list of windows
-list = wmctrl.Window.list()
-
-#Find mapviz window
-for window in list:
-    if window.wm_class == 'start_screen.py.start_screen.py':
-        print(window)
